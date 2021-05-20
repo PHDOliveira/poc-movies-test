8d3504bdc286bb1b9ae06810ce3d07a1ed48b333
Paulo Henrique <ph.phdo@gmail.com>
2021-05-20 16:53:58 -0300

    feat: adiciona função para definir os ids dos chips de forma a não repetir ids

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,3 +17,10 @@ export class AppComponent {
     this.chips = this.chips.filter((el: any) => +el.id !== +id);
     this.moviesService.list$ = this.chips.map((el: any) => el.name);
 }
+
+}
+
+function setNewChipId(chips: []) {
+  if (!(chips.length > 0)) return 0;
+  return chips.map((chip: any) => chip.id).sort((a, z) => (z - a))[0] + 1;
+}
